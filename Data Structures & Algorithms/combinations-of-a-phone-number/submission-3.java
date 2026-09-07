472caa0d3cfeac5d4f5191d490c62535b374689e
Ansh Mishra <am3350704@gmail.com>
2026-09-07 19:47:11 +0530

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result=new ArrayList<>();
+        if(digits==null || digits.length()==0)
+        {
+            return result;
+        }
+        HashMap<Character,String> map = new HashMap<>();
+        map.put('3',"def");
+        map.put('2',"abc");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        backtrack(map,result,digits,0,new StringBuilder());
+        return result;
+        }
+        private void backtrack(HashMap<Character,String> map,List<String> result,String digits,int index,StringBuilder current)
+        {
+            if(index==digits.length())
+            {
+                result.add(current.toString());
+                return;
+            }
+            String letters=map.get(digits.charAt(index));
+            for(char c : letters.toCharArray())
+            {
+                current.append(c);
+                backtrack(map,result,digits,index+1,current);
+                current.deleteCharAt(current.length()-1);
+            }
+        }
+}
